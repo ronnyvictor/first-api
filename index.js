aea446da17c71332ca499ffc4ec74d3f93cf726a
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-01-31 13:14:18 -0500

    saved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send('My first Express API... works!')
+    response.send('My Express API... works!')
 })
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
